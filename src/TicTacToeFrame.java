416acc6770ef31f1e988adccebb0cf563c4d7829
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-06 19:59:38 -0400

    JavaDoc complete

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -3,25 +3,55 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Creates an extension of the JFrame class called TicTacToeFrame. Acts as a
+ * GUI for the logic in TicTacToeRunner.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class TicTacToeFrame extends javax.swing.JFrame
 {
+    //A JPanel containing every other element in the JFrame
     JPanel mainPnl;
+
+    //A JPanel containing a JLabel with the title of the game
     JPanel titlePnl;
+
+    //A JPanel containing the Tic Tac Toe 'board' of JButtons
     JPanel ticTacToePnl;
+
+    //A JPanel containing buttons for clearing the board and quitting the game
     JPanel controlPnl;
 
+    //A JLabel containing the title of the game
     JLabel titleLbl;
 
+    //A 2D array of TicTacToeButton objects for storing move locations on the board
     TicTacToeButton[][] board;
+
+    //A JButton for quitting the game
     JButton quitBtn;
+
+    //A JButton for clearing the board
     JButton clearBtn;
 
+    /**
+     * This constructor determines the basic settings for the TicTacToeFrame and also calls
+     * all the methods that establish the individual panels in the frame.
+     */
     public TicTacToeFrame()
     {
         super("Tic Tac Toe");
+
+        //This Toolkit is used to find the screen size of the computer running the GUI
         Toolkit kit = Toolkit.getDefaultToolkit();
+
+        //This Dimension stores the screen size
         Dimension screenSize = kit.getScreenSize();
+
+        //This int stores the height of the screen
         int screenHeight = screenSize.height;
+
+        //This int stores the width of the screen
         int screenWidth = screenSize.width;
 
         mainPnl = new JPanel();
@@ -37,7 +67,6 @@ public class TicTacToeFrame extends javax.swing.JFrame
         createControlPnl();
         mainPnl.add(controlPnl, BorderLayout.SOUTH);
 
-
         setSize(screenWidth * 3/4, screenHeight * 3/4);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,8 +74,12 @@ public class TicTacToeFrame extends javax.swing.JFrame
         setVisible(true);
     }
 
+    /**
+     * This method clears the JButton 2D array of any Xs or Os
+     */
     public void clearBoard()
     {
+        //This algorithm iterates through every row and column in the board array and sets the text to a blank space
         for(int row = 0; row < 3; row++)
         {
             for(int col = 0; col < 3; col++)
@@ -56,11 +89,20 @@ public class TicTacToeFrame extends javax.swing.JFrame
         }
     }
 
+    /**
+     * This method is used so the TicTacToeRunner can set the GUI to reflect which spaces are taken.
+     * @param row the row location of the taken space
+     * @param col the column location of the taken space
+     * @param player the player (X or O)
+     */
     public void setButton(int row, int col, String player)
     {
         board[row][col].setText(player);
     }
 
+    /**
+     * This method establishes the titlePnl and gives it a JLabel.
+     */
     private void createTitlePnl()
     {
         titlePnl = new JPanel();
@@ -68,6 +110,9 @@ public class TicTacToeFrame extends javax.swing.JFrame
         titlePnl.add(titleLbl);
     }
 
+    /**
+     * This method establishes the ticTacToePnl, its button array, and the ActionListener that determines how input is handled.
+     */
     private void createTicTacToePnl()
     {
         ticTacToePnl = new JPanel();
@@ -75,8 +120,13 @@ public class TicTacToeFrame extends javax.swing.JFrame
 
         board = new TicTacToeButton[3][3];
 
+        /**
+         * This TicTacToeBtnListener uses the actionPerformed method to track which button on the board
+         * was pressed and sends the input to the runner.
+         */
         class ticTacToeBtnListener implements ActionListener
         {
+            //This method tracks which button on the board was pressed and sends the input to the runner
             @Override
             public void actionPerformed(ActionEvent ae)
             {
@@ -87,8 +137,10 @@ public class TicTacToeFrame extends javax.swing.JFrame
             }
         }
 
+        //This creates an instance of the TicTacToeBtnListener
         ticTacToeBtnListener listener = new ticTacToeBtnListener();
 
+        //This algorithm creates TicTacToeButtons inside each index of the board array, sets their text to blank spaces, and adds ActionListeners
         for(int row = 0; row < 3; row++)
         {
             for(int col = 0; col < 3; col++)
@@ -101,6 +153,9 @@ public class TicTacToeFrame extends javax.swing.JFrame
         }
     }
 
+    /**
+     * This method establishes the controlPnl and the clear and quit buttons that go inside it.
+     */
     private void createControlPnl()
     {
         controlPnl = new JPanel();
@@ -110,9 +165,13 @@ public class TicTacToeFrame extends javax.swing.JFrame
         quitBtn = new JButton("Quit");
 
         controlPnl.add(clearBtn);
+
+        //This ActionListener calls the clearBoard() method in the runner
         clearBtn.addActionListener((ActionEvent ae) -> TicTacToeRunner.clearBoard());
 
         controlPnl.add(quitBtn);
+
+        //This ActionListener closes the game and notifies the player
         quitBtn.addActionListener((ActionEvent ae) -> {
             JOptionPane.showMessageDialog(null, "You have quit the game.");
             System.exit(0);
@@ -120,11 +179,23 @@ public class TicTacToeFrame extends javax.swing.JFrame
     }
 }
 
+/**
+ * This class creates a type of JButton called a TicTacToeButton, which tracks its own row and column index.
+ */
 class TicTacToeButton extends JButton
 {
+    //This int tracks the row location of the JButton
     private int row;
+
+    //This int tracks the column location of the JButton
     private int col;
 
+    /**
+     * This constructor establishes the TicTacToeButton with all the normal traits of a JButton,
+     * but also row and column characteristics.
+     * @param row the button's row index
+     * @param col the button's column index
+     */
     public TicTacToeButton(int row, int col)
     {
         super();
@@ -132,10 +203,18 @@ class TicTacToeButton extends JButton
         this.col = col;
     }
 
+    /**
+     * This method returns the row index of the button.
+     * @return an int representing the row index of the button.
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * This method returns the row index of the button.
+     * @return an int representing the row index of the button.
+     */
     public int getCol() {
         return col;
     }
--- src/TicTacToeRunner.java
+++ src/TicTacToeRunner.java
@@ -4,18 +4,38 @@ import javax.swing.*;
 //make UML diagram
 
 /**
+ * Contains all the program logic for running the TicTacToeFrame object, including logging move locations,
+ * testing for wins, and testing for ties.
  * @author wulft
+ * @author Zoe Aspenns aspennza@mail.uc.edu
  */
 public class TicTacToeRunner
 {
+    //An int for setting the permanent row count of the board array
     private static final int ROW = 3;
+
+    //An int for setting the permanent column count of the board array
     private static final int COL = 3;
+
+    //A 2D array for storing move locations
     private static String[][] board = new String[ROW][COL];
+
+    //A String for storing which player (X or O) is active
     private static String player = "X";
+
+    //An instance of TicTacToeFrame
     private static TicTacToeFrame frame;
+
+    //An int for storing the number of moves that have occurred
     private static int moveCnt = 0;
+
+    //An int storing the number of moves necessary before checking for wins
     private static final int MOVES_FOR_WIN = 5;
+
+    //An int storing the number of moves necessary before checking for ties
     private static final int MOVES_FOR_TIE = 7;
+
+    //A boolean tracking whether the game is still active or not
     private static boolean playing = true;
 
     /**
@@ -26,6 +46,9 @@ public class TicTacToeRunner
         clearBoard();
     }
 
+    /**
+     * A method for emptying the Tic Tac Toe array and GUI of any moves.
+     */
     public static void clearBoard()
     {
         // sets all the board elements to a space
@@ -42,17 +65,32 @@ public class TicTacToeRunner
         player = "X";
     }
 
+    /**
+     * A method for checking whether a move is allowed based on whether the move location is empty or not.
+     * @param row the row selected by the player
+     * @param col the column selected by the player
+     * @return a boolean value representing whether the move is valid
+     */
     private static boolean isValidMove(int row, int col)
     {
+        //A boolean tracking whether a given move is valid
         boolean retVal = false;
+
+        //This algorithm checks if the selected space is empty
         if(board[row][col].equals(" "))
             retVal = true;
 
         return retVal;
-
     }
+
+    /**
+     * A method for checking if the current player has achieved a win.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether a win was achieved
+     */
     private static boolean isWin(String player)
     {
+        //An algorithm for running through possible win conditions
         if(isColWin(player) || isRowWin(player) || isDiagnalWin(player))
         {
             return true;
@@ -60,6 +98,12 @@ public class TicTacToeRunner
 
         return false;
     }
+
+    /**
+     * A method for checking whether the current player has achieved a column win.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether a column win was achieved
+     */
     private static boolean isColWin(String player)
     {
         // checks for a col win for specified player
@@ -74,6 +118,12 @@ public class TicTacToeRunner
         }
         return false; // no col win
     }
+
+    /**
+     * A method for checking if the current player has achieved a row win.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether a row win was achieved
+     */
     private static boolean isRowWin(String player)
     {
         // checks for a row win for the specified player
@@ -88,6 +138,12 @@ public class TicTacToeRunner
         }
         return false; // no row win
     }
+
+    /**
+     * A method for checking if a diagonal win was achieved by the current player.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether the current player has achieved a diagonal win
+     */
     private static boolean isDiagnalWin(String player)
     {
         // checks for a diagonal win for the specified player
@@ -107,10 +163,16 @@ public class TicTacToeRunner
         return false;
     }
 
+    /**
+     * A method for testing if a tie has been achieved because the board is full (in the absence of a win or earlier tie).
+     * @return a boolean representing whether a full board tie was achieved.
+     */
     private static boolean isFullBoardTie()
     {
+        //A boolean for checking if there is an empty space on the board
         boolean emptyVal = false;
 
+        //An algorithm for checking if any empty spaces remain on the board
         for(int row=0; row < ROW; row++)
         {
             for(int col=0; col < COL; col++)
@@ -122,6 +184,7 @@ public class TicTacToeRunner
             }
         }
 
+        //An algorithm determining that if there are no empty values, a full board tie is achieved
         if(!emptyVal)
         {
             return true;
@@ -133,6 +196,10 @@ public class TicTacToeRunner
 
     // checks for a tie before board is filled.
     // check for the win first to be efficient
+    /**
+     * A method for checking whether a tie has occurred before the board is full.
+     * @return a boolean representing whether the players have tied.
+     */
     private static boolean isTie()
     {
         boolean xFlag = false;
@@ -228,10 +295,17 @@ public class TicTacToeRunner
         return true;
     }
 
+    /**
+     * A method for taking input from the GUI and calling all other in-game methods to log moves and check for wins and ties.
+     * @param row the row selected by the player in the GUI
+     * @param col the column selected by the player in the GUI
+     */
     public static void buttonAction(int row, int col)
     {
+        //A String for tracking which player played last (i.e., before the current players were switched)
         String prevPlayer = player;
 
+        //This algorithm takes input from the player and checks whether the move is valid
         if(!playing)
         {
             JOptionPane.showMessageDialog(frame, "The game is over. Select Play Again to replay!");
@@ -244,6 +318,7 @@ public class TicTacToeRunner
             frame.setButton(row, col, player);
             moveCnt++;
 
+            //This algorithm switches between the players
             if(player.equals("X"))
             {
                 player = "O";
@@ -254,8 +329,10 @@ public class TicTacToeRunner
             }
         }
 
+        //This algorithm checks if enough moves have passed for a potential win, then checks for wins
         if(moveCnt >= MOVES_FOR_WIN)
         {
+            //This algorithm checks for wins, and, if one is achieved, announces it and shuts down the game
             if(isWin(prevPlayer))
             {
                 JOptionPane.showMessageDialog(frame, "Player " + prevPlayer + " Wins!");
@@ -263,8 +340,11 @@ public class TicTacToeRunner
                 JOptionPane.showMessageDialog(null, "Click Play Again to clear the board and play the game again!");
             }
         }
+
+        //This algorithm checks if enough moves have passed for a potential tie, then checks for full board ties
         if(moveCnt >= MOVES_FOR_TIE && playing)
         {
+            //This algorithm checks for full board ties, and, if one is achieved, announces it and shuts down the game
             if(isFullBoardTie())
             {
                 JOptionPane.showMessageDialog(frame ,"The game has tied! The board is full.");
@@ -273,8 +353,10 @@ public class TicTacToeRunner
             }
         }
 
+        //This algorithm checks if enough moves have passed for a potential tie, then checks for non-full-board ties
         if(moveCnt >= MOVES_FOR_TIE && playing)
         {
+            //This algorithm checks for non-full-board ties, and, if one is achieved, announces it and shuts down the game
             if(isTie())
             {
                 JOptionPane.showMessageDialog(frame, "The game has come to a tie before the board is full!");
